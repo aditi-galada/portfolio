8cc606b8db803e8d4f1ed6b7dc58865fec87621c
aditi-galada <35224741+aditi-galada@users.noreply.github.com>
2021-09-12 01:46:41 -0400

    Update GarmentConstruction.js

--- GarmentConstruction.js
+++ GarmentConstruction.js
@@ -46,7 +46,7 @@ var btn9 = document.getElementById("btn9");
 btn9.onclick = function() {
   modal9.style.display = "block";
 }
-var modal20 = document.getElementById("myModal10");
+var modal10 = document.getElementById("myModal10");
 var btn10 = document.getElementById("btn10");
 btn10.onclick = function() {
   modal10.style.display = "block";
